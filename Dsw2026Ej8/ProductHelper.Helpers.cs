476faea8b8902cacb678e9affe8af58d1ba0d321
Gonzalo Gonzalez <gonzalo.n.gonzalez7@gmail.com>
2026-05-04 16:04:36 -0300

    Resolucion Problema 1

---
+++ Dsw2026Ej8/ProductHelper.Helpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static partial class ProductHelper
+    {
+        public static string FormatearPrice(decimal price) => price.ToString("C2"); 
+    }
+}
---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static partial class ProductHelper
+    {
+        public static string ObtenerEtiquetaProducto(long code, string description, decimal price) => $"[{code}] {description} - {FormatearPrice(price)}";
+    }
+}
