2f4a593d7126150b1a2bcf2f4036712743b02ab6
Gonzalo Gonzalez <gonzalo.n.gonzalez7@gmail.com>
2026-05-04 20:24:44 -0300

    Resolucion Problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static class Problema3
+    {
+        public static string CompararCopias(int originalValue, Product product)
+        {
+            int copia = originalValue;
+            Product refeProd = product;
+            copia += 10;
+            refeProd.SetDescription("nueva descripcion");
+
+            return $"{originalValue} - {copia} - {refeProd.GetDescription}";
+
+        }
+    }
+}
