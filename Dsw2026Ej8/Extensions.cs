fe85ff77791a9e22f4bdd3bdb9f3cad6b70dc2ba
Gonzalo Gonzalez <gonzalo.n.gonzalez7@gmail.com>
2026-05-04 23:38:01 -0300

    Resolucion Problema 6

---
+++ Dsw2026Ej8/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static  class Extensions
+    {
+        public static string ToProductCode(this string code)
+        {
+            return (code ?? "SIN-CODIGO")
+              .Trim()
+              .ToUpper()
+              .Replace(" ", "-");
+
+        }
+    }
+}
---
+++ Dsw2026Ej8/Problema6.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema6
+    {
+        public static string NormalizarCodigoProducto(string code)
+        => code.ToProductCode();
+    }
+}
